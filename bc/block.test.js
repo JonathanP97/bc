046d0f14884f5d2192cf0363d1291ea3f0487b71
Jonathan <jonathan_plata@ymail.com>
2018-04-13 23:07:22 -0700

    chain fixed

--- bc/block.test.js
+++ bc/block.test.js
@@ -1,13 +1,14 @@
 const Block = require('./block');
+const {DIFFICULTY} = require('../config');
 
 describe('Block', () => {
   let data, lastBlock, block;
 
   beforeEach(() => {
-  	data = 'bar';
-	lastBlock = Block.genesis();  
-	block = Block.mineBlock(lastBlock, data);	
-  })
+    data = 'bar';
+	  lastBlock = Block.genesis();  
+    block = Block.mineBlock(lastBlock, data);	
+  });
 
   it('sets the `data` to match the input', () => {
   	expect(block.data).toEqual(data);
@@ -15,5 +16,9 @@ describe('Block', () => {
 
   it('sets the `lastHash` to match the hash of the last blcok', () => {
   	expect(block.lastHash).toEqual(lastBlock.hash);
+  });
+
+  it('generates a hash that matches difficulty', () => {
+    expect(block.hash.substring(0, DIFFICULTY)).toEqual('0'.repeat(DIFFICULTY));
   });
 });
